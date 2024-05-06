5398cf3fe9efc8591ef2031dc6524c9de434188d
Bruno Verçosa <69854207+brunoverc@users.noreply.github.com>
2024-05-06 19:22:08 -0300

    Services not implementeds

--- src/Senac.eShop.Web/Services/CatalogService.cs
+++ src/Senac.eShop.Web/Services/CatalogService.cs
@@ -6,40 +6,8 @@ namespace Senac.eShop.Web.Services
 {
     public interface ICatalogService
     {
-        Task<PagedViewModel<ProductViewModel>> GetAll(int pageSize, int pageIndex, string query = null);
-        Task<ProductViewModel> GetById(Guid id);
     }
-    public class CatalogService : Service, ICatalogService
+    public class CatalogService
     {
-        private readonly HttpClient _httpClient;
-
-        public CatalogService(HttpClient httpClient,
-            IOptions<AppSettings> settings)
-        {
-            httpClient.BaseAddress = new Uri(settings.Value.Url);
-
-            _httpClient = httpClient;
-        }
-
-        public async Task<ProductViewModel> GetById(Guid id)
-        {
-            var response = await _httpClient.GetAsync($"/product/{id}");
-
-            HandleErrosResponse(response);
-
-            return await DeserializeObjectResponse<ProductViewModel>(response);
-        }
-
-        public async Task<PagedViewModel<ProductViewModel>> GetAll(
-            int pageSize, 
-            int pageIndex, 
-            string productName)
-        {
-            var response = await _httpClient.GetAsync($"/product/{pageSize}/{pageIndex}/{productName}");
-
-            HandleErrosResponse(response);
-
-            return await DeserializeObjectResponse<PagedViewModel<ProductViewModel>>(response);
-        }
     }
 }
--- src/Senac.eShop.Web/Services/ClientService.cs
+++ src/Senac.eShop.Web/Services/ClientService.cs
@@ -8,44 +8,10 @@ namespace Senac.eShop.Web.Services
 {
     public interface IClienteService
     {
-        Task<AddressViewModel> GetAddress();
-        Task<ResponseResult> SetAddress(AddressViewModel address, Guid clientId);
     }
 
-    public class ClienteService : Service, IClienteService
+    public class ClienteService
     {
-        private readonly HttpClient _httpClient;
-
-        public ClienteService(HttpClient httpClient, IOptions<AppSettings> settings)
-        {
-            _httpClient = httpClient;
-            _httpClient.BaseAddress = new Uri(settings.Value.Url);
-        }
-
-        public async Task<AddressViewModel> GetAddress()
-        {
-            var response = await _httpClient.GetAsync("/client");
-
-            if (response.StatusCode == HttpStatusCode.NotFound) return null;
-
-            HandleErrosResponse(response);
-
-            return await DeserializeObjectResponse<AddressViewModel>(response);
-        }
-
-        public async Task<ResponseResult> SetAddress(AddressViewModel address, Guid clientId)
-        {
-            var enderecoContent = GetContent(address);
-
-            var response = await _httpClient.
-                PostAsync($"/client/set-address-client/{clientId}/", enderecoContent);
-
-            if (!HandleErrosResponse(response))
-            {
-                return await DeserializeObjectResponse<ResponseResult>(response);
-            }
-
-            return ReturnOk();
-        }
+       
     }
 }
